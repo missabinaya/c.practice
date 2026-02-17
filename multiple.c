1f3b7f2b6b1ac444ca1a73d7cb5fd9a8f8452331
Abinaya <abinayaeswari71@gmail.com>
2026-02-17 12:03:51 +0530

    multiplication program

---
+++ multiple.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+
+    float a, b, c;
+    scanf("%f", &a);
+    sacnf("%f", &b);
+    scanf("%f", &c);
+    mult = a * b * c;
+    printf("Result:%f", mult);
+    
+}
